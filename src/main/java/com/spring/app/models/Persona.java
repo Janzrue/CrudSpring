ba6b40261a8fc871dfe1f0ad56f904aa6c164da9
Juan <juanmaperez5826@gmail.com>
2024-06-06 21:44:27 -0500

    First commit

--- src/main/java/com/spring/app/models/Persona.java
+++ src/main/java/com/spring/app/models/Persona.java
@@ -5,22 +5,18 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 public class Persona {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Getter
-    @Setter
     private long id;
 
     @Column(name="name")
-    @Getter
-    @Setter
     private String nombre;
 
     @Column(name="phone")
-    @Getter
-    @Setter
     private String telefono;
 }
