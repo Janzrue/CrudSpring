87f2fe814fbf8b37866f960c4da054d9eed264cb
Juan <juanmaperez5826@gmail.com>
2024-07-24 14:58:07 -0500

    Create GetUserByID

--- src/main/java/com/spring/app/service/Service.java
+++ src/main/java/com/spring/app/service/Service.java
@@ -1,4 +1,4 @@
-package com.spring.app.controllers;
+package com.spring.app.service;
 
 import com.spring.app.models.User;
 import com.spring.app.repository.Repository;
@@ -8,36 +8,31 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@RestController
-public class Controller {
+@org.springframework.stereotype.Service
 
+public class Service {
     @Autowired
     private Repository repositorio;
 
 
-    @GetMapping()
-    public String index(){
+    public String index() {
         return "CONECTADO";
     }
 
-    @GetMapping("users")
-    public List<User> getAllUsers(){
+    public List<User> getAllUsers() {
         return repositorio.findAll();
     }
 
-    @GetMapping("users/{id}")
-    public Optional<User> getUserByID(@PathVariable Long id){
+    public Optional<User> getUserByID(Long id) {
         return repositorio.findById(id);
     }
 
-    @PostMapping("users")
-    public String createUser(@RequestBody User user){
+    public String createUser(@RequestBody User user) {
         repositorio.save(user);
         return "Usuario creado";
     }
 
-    @PutMapping("users/{id}")
-    public String updateUser(@PathVariable Long id, @RequestBody User user){
+    public String updateUser(@PathVariable Long id, @RequestBody User user) {
         User updatePersona = repositorio.findById(id).get();
         updatePersona.setName(user.getName());
         updatePersona.setPhone(user.getPhone());
@@ -45,10 +40,10 @@ public class Controller {
         return "Usuario editado";
     }
 
-    @DeleteMapping("users/{id}")
-    public String deleteUser(@PathVariable Long id){
+    public String deleteUser(@PathVariable Long id) {
         User deleteUser = repositorio.findById(id).get();
         repositorio.delete(deleteUser);
         return "Usuario eliminado";
     }
+
 }
